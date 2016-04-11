d1eb6e688512d528335af505d9d02c03622a5f5a
aratsu <aerodynamics.a@gmail.com>
2016-04-11 12:25:45 +0900

    mycat追加

--- bottle_test/hello.cpp
+++ bottle_test/hello.cpp
@@ -1,12 +1,30 @@
 // hello.c
 #include <cstdio>
-#include <cstring>
+#include <iostream>
+#include <fstream>
 using namespace std;
 
 int add(int x, int y){
 	return x + y;
 }
 
-void out(const char* firstname, const char* lastname, char* const str_out, const size_t len){
-	snprintf(str_out, len, "Hello, %s %s!", firstname, lastname);
+void out(const char* firstname, const char* lastname, char* const strout, const size_t len){
+	snprintf(strout, len, "Hello, %s %s!", firstname, lastname);
+}
+
+int mycat(const char* filename, char* const strout, const size_t len)
+{
+	int i;
+	char ch;
+	ifstream ifs(filename);
+	if(!ifs) {
+		fprintf(stderr, "Can't open file: \"%s\"\n", filename);
+		return -1;
+	}
+
+	for(i = 0; i < (int)len-1 && ifs >> ch; i++) {
+		strout[i] = ch;
+	}
+	strout[i] = '\0';
+	return i;
 }
--- bottle_test/helloWrap.cpp
+++ bottle_test/helloWrap.cpp
@@ -1,9 +1,10 @@
 // helloWrap.c
 #include <python3.4/Python.h>
-#define BUFSIZE 256
+#define BUFSIZE 2048
 
 extern int add(int, int);
 extern void out(const char*, const char*, char* const, const size_t);
+extern int mycat(const char*, char* const, const size_t);
 
 static PyObject* hello_add(PyObject* self, PyObject* args){
 	int x, y, g;
@@ -18,23 +19,39 @@ static PyObject* hello_add(PyObject* self, PyObject* args){
 
 static PyObject* hello_out(PyObject* self, PyObject* args, PyObject* kw){
 
-	const char* adrs = NULL;
-	const char* name = NULL;
-	static char* argnames[] = {(char*)"adrs", (char*)"name", NULL};
-	char str_out[BUFSIZE];
+	const char* fname = NULL;
+	const char* lname = NULL;
+	static char* argnames[] = {(char*)"fname", (char*)"lname", NULL};
+	char strout[BUFSIZE];
 
-	if(!PyArg_ParseTupleAndKeywords(args, kw, "|ss", argnames, &adrs, &name)){
+	if(!PyArg_ParseTupleAndKeywords(args, kw, "|ss", argnames, &fname, &lname)){
 		return NULL;
 	}else{
-		out(adrs, name, str_out, sizeof(str_out));
-		return Py_BuildValue("s", str_out);
+		out(fname, lname, strout, sizeof(strout));
+		return Py_BuildValue("s", strout);
 	}
 }
 
+static PyObject* hello_mycat(PyObject* self, PyObject* args, PyObject* kw){
+
+	const char* filename = NULL;
+	static char* argnames[] = { (char*)"filename", NULL};
+	char strout[BUFSIZE];
+
+	if(!PyArg_ParseTupleAndKeywords(args, kw, "|s", argnames, &filename)){
+		return NULL;
+	}else{
+		if(mycat(filename, strout, sizeof(strout)) < 0) {
+			return Py_BuildValue("s", "Error");
+		}
+		return Py_BuildValue("s", strout);
+	}
+}
 
 static PyMethodDef hellomethods[] = {
 	{"add", (PyCFunction)hello_add, METH_VARARGS},
 	{"out", (PyCFunction)hello_out, METH_VARARGS | METH_KEYWORDS},
+	{"mycat", (PyCFunction)hello_mycat, METH_VARARGS | METH_KEYWORDS},
 	{NULL},
 };
 
