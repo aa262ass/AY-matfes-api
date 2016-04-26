61883fc1688cf7db8b894f172c1b1226817198fb
aratsu <aerodynamics.a@gmail.com>
2016-04-26 20:17:03 +0900

    fscanfを使う場合

---
+++ c++/read.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#define Feats_dim 99120
+using namespace std;
+
+const char *filepath = "./result/descriptor.csv";
+
+int main()
+{
+	FILE *fp = fopen(filepath, "r");
+	char name[1024];
+	double val;
+
+	while(fscanf(fp, "%[^,]", name) != EOF) {
+		printf("%s", name);
+		for(int i = 0; i < FEATS_DIM; i++) {
+			fscanf(fp, ",%lf", &val);
+			printf(" %f", val);
+		}
+		fscanf(fp, "\n");
+		printf("\n");
+	}
+
+	fclose(fp);
+	return 0;
+}
