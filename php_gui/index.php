c5b453b01fbaf41427553265527ae14c8103afb3
kt-tsubota <tsubo.tsubo.0219@gmail.com>
2016-05-06 00:22:14 +0900

    php

---
+++ php_gui/index.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+  <form action = "./result/upfiles.php" method="POST"  enctype="multipart/form-data">
+    <input type="file" name="bitmap">
+    <input type="submit" value="Upload" class="btn btn-primary">
+  </form>
+</body>
+</html>
---
+++ php_server/index.php
@@ -0,0 +1,9 @@
+<?php
+$target_dir = "./images/";
+$target_path = $target_dir . basename($_FILES['bitmap']['name']);
+if(move_uploaded_file($_FILES['bitmap']['tmp_name'], $target_path)){
+  echo "The file " . basename($_FILES['f1']['name']) . "has been uploaded";
+}else{
+  echo "error";
+}
+?>
