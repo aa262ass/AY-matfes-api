9c568ade156ec1c8a1302390cfbca62c7258bc73
aratsu <aerodynamics.a@gmail.com>
2016-05-07 02:11:09 +0900

    numpy使用

--- index.py
+++ index.py
@@ -1,58 +1,44 @@
-# -*- coding: utf-8 -*
 from bottle import route, run, template, request, static_file, HTTPResponse
+from datetime import datetime
 import webbrowser as web
-import os
-import subprocess
-import csv
-from math import sqrt
+import numpy as np
+import os, subprocess
 
-csvPath = 'database/database.csv'
+dataPath = 'database/database.npz'
 featsDim = 99120
-dataSize = 0
-dataset = []
-dataName = []
+category = ['allData', 'mElse', 'mActor', 'mArtist', 'mShowman', \
+                'fElse', 'fActor', 'fArtist', 'fShowman']
+categorySize = len(category)
+dataSet = []
 dataID = []
 
+
 def init():
-  global dataset
-  global dataName
+  global dataSet
   global dataID
-  global dataSize
-  f = open(csvPath, 'r')
-  reader = csv.reader(f)
+  tmpData = np.load(dataPath)
 
-  for myline in reader:
-    mydata = map(float, myline[2:])
-    norm = calcNorm(mydata)
-    for i in range(featsDim):
-      mydata[i] /= norm
-    dataset.append(mydata)
-    dataName.append(myline[0].decode('utf-8'))
-    dataID.append(int(myline[1]))
-  
-  dataSize = len(dataset)
+  for key in category:
+    dataSet.append(tmpData[key])
 
-
-def calcNorm(mydata):
-  s = 0.0
-  for i in range(featsDim):
-    s += mydata[i]**2
-  return sqrt(s)
-
-
-def calcSim(data1, data2):
-  rtn = 0.0
-  for i in range(featsDim):
-    rtn += data1[i] * data2[i]
-  return rtn
+  dataID.append(range(1001,1089)+range(1101,1153)+range(1201,1242)+range(1301,1369) \
+                      +range(2001,2053)+range(2101,2182)+range(2201,2254)+range(2301,2328))
+  dataID.append(range(1001, 1089))
+  dataID.append(range(1101, 1153))
+  dataID.append(range(1201, 1242))
+  dataID.append(range(1301, 1369))
+  dataID.append(range(2001, 2053))
+  dataID.append(range(2101, 2182))
+  dataID.append(range(2201, 2254))
+  dataID.append(range(2301, 2328))
 
 
 def cpp_exec(filename):
   cmd = ["./temp", "./image/"+filename]
   result = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
   if len(result) == 0:
-    return []
-  return map(float, result.decode('utf-8').split(','))
+    return np.array([])
+  return np.array(map(float, result.decode('utf-8').split(',')))
 
 
 @route('/title')
@@ -62,45 +48,41 @@ def title():
 
 @route('/result', method='POST')
 def result():
+  # time1 = datetime.now()
   upload = request.files.get('bitmap')
+  '''
   fname, extname = os.path.splitext(upload.filename)
 
   upload.filename = 'test' + extname
 
   if extname not in (".png", ".jpg"):
       return "The file extension is not allowed."
-
+  '''
   upload.save("./image", overwrite=True)
+  # time2 = datetime.now()
   feats = cpp_exec(upload.filename)
 
-  if len(feats) == 0:
-    body = 'no face detected.'
+  # time3 = datetime.now()
+  if feats.size == 0:
+    body = ','.join(['0']*categorySize)
+  else:
+    feats = feats.reshape(featsDim, 1)
+    body = '' 
+    for i in xrange(categorySize):
+      body += ',' if i != 0 else ''
+      body += str(dataID[i][np.argmax(dataSet[i].dot(feats))])
 
-  else: 
-    norm = calcNorm(feats)
-    for i in range(featsDim):
-      feats[i] /= norm
-    maxSim = -1.0
-    maxName = ''
-    maxID = 0
-    for i in range(dataSize):
-      sim = calcSim(feats, dataset[i])
-      if sim > maxSim:
-        maxSim = sim
-        maxName = dataName[i]
-        maxID = dataID[i]
-    body = str(maxID) + '/' + str(maxSim)
+  #time4 = datetime.now()
 
+  '''
+  body += '\n'
+  body += str(time2-time1) + '\n'
+  body += str(time3-time2) + '\n'
+  body += str(time4-time3) + '\n'
+  '''
   r = HTTPResponse(status=200, body=body)
   r.set_header('Content-Type', 'text/plain')
   return r
-
-
-  #os.remove("./image/pic.png")
-
-  #return template('show', name=fname)
-  #return "Request permitted!"
-  #here must be a number
 
 
 if __name__ == '__main__':
