51f209e47c33b87e08c43b5056fa8ee6f61b2977
aratsu <aerodynamics.a@gmail.com>
2016-05-06 14:48:16 +0900

    実際のデータセットに合わせてみた

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*
 from bottle import route, run, template, request, static_file, HTTPResponse
 import webbrowser as web
 import os
@@ -7,23 +8,29 @@ from math import sqrt
 
 csvPath = 'database/database.csv'
 featsDim = 99120
-dataSize = 500
+dataSize = 0
 dataset = []
-dataname = []
+dataName = []
+dataID = []
 
 def init():
   global dataset
-  global dataname
+  global dataName
+  global dataID
+  global dataSize
   f = open(csvPath, 'r')
   reader = csv.reader(f)
 
   for myline in reader:
-    mydata = map(float, myline[1:])
+    mydata = map(float, myline[2:])
     norm = calcNorm(mydata)
     for i in range(featsDim):
       mydata[i] /= norm
     dataset.append(mydata)
-    dataname.append(myline[0])
+    dataName.append(myline[0].decode('utf-8'))
+    dataID.append(int(myline[1]))
+  
+  dataSize = len(dataset)
 
 
 def calcNorm(mydata):
@@ -43,6 +50,8 @@ def calcSim(data1, data2):
 def cpp_exec(filename):
   cmd = ["./temp", "./image/"+filename]
   result = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()[0]
+  if len(result) == 0:
+    return []
   return map(float, result.decode('utf-8').split(','))
 
 
@@ -56,27 +65,32 @@ def result():
   upload = request.files.get('bitmap')
   fname, extname = os.path.splitext(upload.filename)
 
-  if (fname == ""):
-      fname = "hogefile"
+  upload.filename = 'test' + extname
 
   if extname not in (".png", ".jpg"):
       return "The file extension is not allowed."
 
   upload.save("./image", overwrite=True)
   feats = cpp_exec(upload.filename)
-  norm = calcNorm(feats)
-  for i in range(featsDim):
-    feats[i] /= norm
 
-  maxSim = -1.0
-  maxName = ''
-  for i in range(dataSize):
-    sim = calcSim(feats, dataset[i])
-    if sim > maxSim:
-      maxSim = sim
-      maxName = dataname[i]
+  if len(feats) == 0:
+    body = 'no face detected.'
 
-  body = maxName + '/' + str(maxSim)
+  else: 
+    norm = calcNorm(feats)
+    for i in range(featsDim):
+      feats[i] /= norm
+    maxSim = -1.0
+    maxName = ''
+    maxID = 0
+    for i in range(dataSize):
+      sim = calcSim(feats, dataset[i])
+      if sim > maxSim:
+        maxSim = sim
+        maxName = dataName[i]
+        maxID = dataID[i]
+    body = str(maxID) + '/' + str(maxSim)
+
   r = HTTPResponse(status=200, body=body)
   r.set_header('Content-Type', 'text/plain')
   return r
